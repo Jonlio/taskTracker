4652f04fef310a67817484906f438e19075182d9
Jo <jo-lion@hotmail.fr>
2021-05-30 17:16:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-container class="mainContainer">
+
+    <el-aside width="200px">
+      <TheMenu />
+    </el-aside>
+
+    <el-container>
+      
+      <el-header height="60px">
+        <TheTopTask />
+      </el-header>
+
+      <el-main>
+        <TaskList :tasks="tasks" />
+      </el-main>
+
+    </el-container>
+
+  </el-container>
+</template>
+
+<script>
+  import TheMenu from './components/TheMenu.vue'
+  import TheTopTask from './components/TheTopTask.vue'
+  import TaskList from './components/TaskList.vue'
+
+  export default {
+    components: {
+      TheMenu,
+      TheTopTask,
+      TaskList
+    },
+    data() {
+      const sample = {
+        name: 'Développement de la feature "edit" d\'un tweet',
+        startTime: '01/03/2021',
+        endTime: '00:43:13',
+      };
+      return {
+        tasks: Array(20).fill(sample)
+      }
+    }
+  };
+</script>
+
+<style lang="scss">
+body { margin: 0; }
+#app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.mainContainer { height: 100%; }
+
+.el-aside {
+  height: 100%;
+  color: #333;
+  border-right: solid 1px #e6e6e6;
+}
+.el-header {
+  padding: 0 !important;
+  border-bottom: solid 1px #e6e6e6;
+  color: #333;
+  line-height: 60px;
+  .el-input .el-input__inner {
+    border: none !important;
+  }
+}
+
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-table
+    :data="tasks"
+    stripe
+    style="width: 100%">
+
+    <el-table-column
+      prop="name"
+      label="Tâche">
+    </el-table-column>
+
+    <el-table-column
+      align="right"
+      prop="startTime"
+      label="Début et fin"
+      width="100">
+      <template #header></template>
+    </el-table-column>
+
+    <el-table-column
+      align="right"
+      prop="endTime"
+      label="Durée"
+      width="100">
+      <template #header></template>
+    </el-table-column>
+
+    <el-table-column
+      align="right"
+      label="Actions"
+      width="200">      
+      <template #header></template>
+      <template #default="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">Editer</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">Supprimer</el-button>
+      </template>
+    </el-table-column>
+    
+  </el-table>
+</template>
+
+<script>
+  export default {
+    props: {
+      tasks: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+    }
+  }
+</script>
