1e9fe2dbdf0570740cefef048bec9c20038ba404
Jo <jo-lion@hotmail.fr>
2021-06-17 21:00:40 +0200

    ascending

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,8 +1,16 @@
+  
 <template>
+
+  <el-select v-model="sortBy" placeholder="Ordre des tâches">
+    <el-option label="La plus récente d'abord" value="descending"></el-option>
+    <el-option label="La plus ancienne d'abord" value="ascending"></el-option>
+  </el-select>
+
   <el-table
     :data="tasks"
-    stripe
+    :row-class-name="checkHighlight"
     row-key="id"
+    @row-click="setHighlight"
     empty-text="Aucune tâche"
     style="width: 100%"
     v-loading="areTasksLoading"
@@ -30,7 +38,6 @@
       label="Durée"
       width="100">
       <template #header></template>
-      <!--souci-->
       <template #default="scope">
         {{ durationBetweenTimestamps(scope.row.startTime, scope.row.endTime) }}
       </template>
@@ -64,7 +71,31 @@
     },
     data() {
       return {        
-        tsFormatter: Intl.DateTimeFormat('fr', { hour: '2-digit', minute: '2-digit' })
+        tsFormatter: Intl.DateTimeFormat('fr', { hour: '2-digit', minute: '2-digit' }),
+        defaultSortBy: 'descending',
+        sortBy: (this.$route.query.sortBy === 'ascending') ? 'ascending' : 'descending'
+      }
+    },
+    props: {
+      tasks: {
+        type: Array,
+        default: []
+      },
+      areTasksLoading: {
+        type: Boolean,
+        default: false
+      }
+    },
+    watch: {
+      sortBy(newVal) {
+        this.$router.push({ query: { sortBy: (newVal === this.defaultSortBy) ? undefined : newVal } })
+        this.sortTable()
+      },
+      tasks: {
+        deep: true,
+        handler() {
+          this.sortTable()
+        }
       }
     },
     methods: {
@@ -87,11 +118,36 @@
       },
       copyToClipboard(text) {
         navigator.clipboard.writeText(text)
+        this.$notify({
+          title: 'Succès',
+          message: `Le nom de cette tâche a bien été copié`,
+          type: 'success',
+          offset: 50,
+          duration: 1500
+        });
+      },
+      sortTable() {
+        this.$refs.table.sort('name', this.sortBy)
+      },
+      checkHighlight({ row }) {
+        if (this.$route.params.taskID && row.id === this.$route.params.taskID) {
+          return 'highlight-line'
+        } else {
+          return ''
+        }
+      },
+      setHighlight(row) {
+        this.$router.push({ path: '/home/' + row.id })
       }
     },
     mounted () {
-      const sortBy = (this.$root.query.sortBy === 'ascending') ? 'ascending' : 'descending';
-      this.$refs.table.sort('name', sortBy);
+      this.sortTable()
     },
   }
 </script>
+
+<style scoped>
+.el-select {
+  float: right;
+}
+</style>
